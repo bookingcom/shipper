e1f5790aba2f31c4d7a3594e1e86656f3d06ee9e
Igor Sutton <igor.sutton@booking.com>
2017-12-28 13:38:27 +0100

    Add /ship endpoint and rudimentary ShipmentRequest type definition

---
+++ acceptance/acceptance.go
@@ -0,0 +1,70 @@
+package acceptance
+
+/*
+apiVersion: shipper/v1
+kind: ShipmentRequest
+metadata:
+  clusterSelectors:
+  - pci
+  - gpu
+  chart:
+    name: 'perl'
+    version: '0.0.1'
+  strategy:
+	name: 'vanguard'
+	spec:
+	  stepCount: '10%'
+	  initialReplicas: 5
+spec:
+  perl:
+	image:
+	  repository: 'myapp'
+	  tag: 'latest'
+
+*/
+
+// ShipmentStrategy is...
+type ShipmentStrategy struct {
+	Name string
+	Spec map[string]interface{}
+}
+
+// Chart is...
+type Chart struct {
+	Name    string
+	Version string
+}
+
+// ShipmentRequestMeta is...
+type ShipmentRequestMeta struct {
+	ClusterSelectors []string
+	Chart            Chart
+	Strategy         ShipmentStrategy
+}
+
+// ShipmentRequest is...
+type ShipmentRequest struct {
+	APIVersion string
+	Kind       string
+	Meta       ShipmentRequestMeta
+}
+
+// CanAccessTokenShipApplication does...
+func CanAccessTokenShipApplication(appName string, accessToken string) bool {
+	return false
+}
+
+// IsAppRegisteredInServiceDirectory does...
+func IsAppRegisteredInServiceDirectory(name string) bool {
+	return false
+}
+
+// DoesChartExistInChartRepository does...
+func DoesChartExistInChartRepository(chart string, chartVersion string) bool {
+	return false
+}
+
+// DoesImageExistInRegistry does...
+func DoesImageExistInRegistry(repository string, label string) bool {
+	return false
+}
