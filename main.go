4346e5455dc67634411c19c29a830aa47708238d
Igor Sutton <igor.sutton@booking.com>
2017-12-29 11:35:31 +0100

    Add shipHandler

--- main.go
+++ main.go
@@ -6,9 +6,12 @@ import (
 	"net/http"
 	"time"
 
+	"encoding/json"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/bookingcom/gopath/src/booking/tell"
+	"github.com/bookingcom/shipper/shipping"
+	"io/ioutil"
 )
 
 // AccessTokenHeader is the header name where the Passport access token is expected
@@ -20,6 +23,7 @@ func init() {
 
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/api/ship", shipHandler).Methods("POST")
 	router.HandleFunc("/", indexHandler)
 
 	server := createServer(router)
@@ -31,6 +35,39 @@ func main() {
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Shipped!")
+}
+
+func shipHandler(w http.ResponseWriter, r *http.Request) {
+	// The access token is mandatory. If not present, return an error right away.
+	accessToken := r.Header.Get(AccessTokenHeader)
+	if accessToken == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "%s header is required", AccessTokenHeader)
+		return
+	}
+
+	// The application name is mandatory. If not present, return an error right away.
+	appName := r.URL.Query().Get("appName")
+	if appName == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "application name is required")
+	}
+
+	// Extract ShipmentRequest from the body. If an error happens, return it right away.
+	var request shipping.ShipmentRequest
+	b, _ := ioutil.ReadAll(r.Body)
+	if err := json.Unmarshal(b, &request); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "couldn't unmarshal the shipment request")
+	}
+
+	// Try to Ship the application using the extracted ShipmentRequest on behalf of the given access token. If an
+	// error is returned, return it right away.
+	err := shipping.Ship(appName, &request, accessToken)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+	}
 }
 
 func createServer(h http.Handler) *http.Server {
