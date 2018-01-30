d3aa4f3f5931511cd728367d702f6299d8cb64ce
Igor Sutton <igor.sutton@booking.com>
2018-01-30 14:23:38 +0100

    (installationcontroller) WIP: Implementing business logic

--- pkg/controller/installation/businesslogic.go
+++ pkg/controller/installation/businesslogic.go
@@ -2,12 +2,45 @@ package installation
 
 import (
 	shipperV1 "github.com/bookingcom/shipper/pkg/apis/shipper/v1"
+	shipperChart "github.com/bookingcom/shipper/pkg/chart"
+	"gopkg.in/yaml.v2"
+	"k8s.io/helm/pkg/chartutil"
+	"k8s.io/helm/pkg/proto/hapi/chart"
+	"k8s.io/helm/pkg/timeconv"
 )
 
-func (c *Controller) renderChart(chrt shipperV1.EmbeddedChart, cluster *shipperV1.Cluster) ([]string, error) {
-	// TODO: Implement chart rendering
-	// - Render chart for cluster
-	return []string{}, nil
+func (c *Controller) renderRelease(release *shipperV1.Release, cluster *shipperV1.Cluster) ([]string, error) {
+	chrt := loadChartFromEmbedded(release.Environment.Chart) // TODO: Make this a method of EmbeddedChart
+	vals, err := loadValsFromRelease(release)                // TODO: Make this a method of Release
+	if err != nil {
+		return nil, err
+	}
+	options := optionsForCluster(release, cluster)
+	return shipperChart.RenderChart(chrt, vals, options)
+}
+
+func optionsForCluster(release *shipperV1.Release, cluster *shipperV1.Cluster) chartutil.ReleaseOptions {
+	options := chartutil.ReleaseOptions{
+		Name:      release.Name,
+		Time:      timeconv.Now(),
+		Namespace: release.Namespace,
+		IsInstall: true,
+	}
+	return options
+
+}
+
+func loadValsFromRelease(release *shipperV1.Release) (*chart.Config, error) {
+	yamlValues, err := yaml.Marshal(release.Environment.ShipmentOrder.Values)
+	if err != nil {
+		return nil, err
+	}
+	return &chart.Config{Raw: string(yamlValues)}, nil
+}
+
+func loadChartFromEmbedded(embeddedChart shipperV1.EmbeddedChart) *chart.Chart {
+	// base64 -> []byte -> *chart.Chart
+	return nil
 }
 
 func (c *Controller) installIfMissing(rendered []string, cluster *shipperV1.Cluster) error {
@@ -22,7 +55,7 @@ func (c *Controller) clusterBusinessLogic(
 	it *shipperV1.InstallationTarget,
 	cluster *shipperV1.Cluster,
 ) error {
-	rendered, err := c.renderChart(release.Environment.Chart, cluster)
+	rendered, err := c.renderRelease(release, cluster)
 	if err != nil {
 		return err
 	}
