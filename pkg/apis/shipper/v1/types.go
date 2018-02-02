ad5b7e3a9517ae893fd92c9449df5babb26db9d0
Ben Tyler <benjamin.tyler@booking.com>
2018-02-02 13:07:56 +0100

    types: enrichen capacity cluster status
    For now we'll include each failing pod's containers and condition. If
    this turns into a scalability issue we can come up with a way to
    aggregate them into counts.

--- pkg/apis/shipper/v1/types.go
+++ pkg/apis/shipper/v1/types.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
@@ -292,9 +293,21 @@ type CapacityTargetStatus struct {
 }
 
 type ClusterCapacityStatus struct {
-	Name             string `json:"name"`
-	AchievedReplicas uint   `json:"achievedReplicas"`
-	Status           string `json:"status"`
+	Name              string      `json:"name"`
+	AvailableReplicas uint        `json:"achievedReplicas"`
+	SadPods           []PodStatus `json:"sadPods"`
+}
+
+type PodStatus struct {
+	Name       string              `json:"name"`
+	Containers []ContainerStatus   `json:"containers"`
+	Condition  corev1.PodCondition `json:"condition"`
+	Phase      string              `json:"phase"`
+}
+
+type ContainerStatus struct {
+	Name   string `json:"name"`
+	Status string `json:"status"`
 }
 
 // the capacity and traffic controllers need context to pick the right
