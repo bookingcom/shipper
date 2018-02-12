3fbf60d342a62e9dfadc3e0a2530f7d44834b835
Alexey Surikov <alexey.surikov@booking.com>
2018-02-12 15:08:33 +0100

    chart rendering: fix tarball name in tests
    this patch also replaces a bunch of code in favour of using
    repotest.NewTempServer() which prepares the testing environment for us

--- pkg/chart/chart_test.go
+++ pkg/chart/chart_test.go
@@ -1,42 +1,28 @@
 package chart
 
 import (
-	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
 
-	"k8s.io/helm/pkg/helm/helmpath"
 	"k8s.io/helm/pkg/repo/repotest"
 
 	shipperv1 "github.com/bookingcom/shipper/pkg/apis/shipper/v1"
 )
 
 func TestDownload(t *testing.T) {
-	dir, err := ioutil.TempDir("", "shipper-api-downloadchart-")
+	srv, hh, err := repotest.NewTempServer("testdata/*.tgz")
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer os.RemoveAll(dir)
-
-	if err := ensureDirectories(helmpath.Home(dir)); err != nil {
-		t.Fatal(err)
-	}
-
-	srv := repotest.NewServer(dir)
-	defer srv.Stop()
-	if _, err := srv.CopyCharts("testdata/*.tgz"); err != nil {
-		t.Error(err)
-		return
-	}
-	if err := srv.LinkIndices(); err != nil {
-		t.Fatal(err)
-	}
+	defer func() {
+		srv.Stop()
+		os.RemoveAll(hh.String())
+	}()
 
 	inChart := shipperv1.Chart{
 		Name:    "my-complex-app",
-		Version: "0.1.0",
+		Version: "0.2.0",
 		RepoURL: srv.URL(),
 	}
 
@@ -48,7 +34,7 @@ func TestDownload(t *testing.T) {
 
 func TestRender(t *testing.T) {
 	cwd, _ := filepath.Abs(".")
-	chart, err := os.Open(filepath.Join(cwd, "testdata", "my-complex-app-0.1.0.tgz"))
+	chart, err := os.Open(filepath.Join(cwd, "testdata", "my-complex-app-0.2.0.tgz"))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -60,18 +46,4 @@ func TestRender(t *testing.T) {
 	if _, err := Render(chart, "my-complex-app", "my-complex-app", vals); err != nil {
 		t.Fatal(err)
 	}
-}
-
-func ensureDirectories(hh helmpath.Home) error {
-	for _, p := range []string{
-		hh.String(),
-		hh.Repository(),
-		hh.Cache(),
-	} {
-		if err := os.MkdirAll(p, 0755); err != nil {
-			return fmt.Errorf("could not create %s: %s", p, err)
-		}
-	}
-
-	return nil
 }
