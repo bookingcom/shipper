b2d7accfb70ef8746d6b523d6f94af224870a047
Alexey Surikov <alexey.surikov@booking.com>
2018-02-27 18:14:23 +0100

    clusterclientstore: mention informers in the package description

--- pkg/controller/clusterclientstore/doc.go
+++ pkg/controller/clusterclientstore/doc.go
@@ -1,9 +1,9 @@
-// package clusterclientstore provides a thread-safe storage for kubernetes
-// clients. The internal storage is updated automatically by observing
-// the kubernetes cluster for cluster objects. New cluster objects
-// trigger a client creation, updates to Secret objects trigger
-// re-creation of a client, and Cluster deletions cause the removal of
-// a client.
+// Package clusterclientstore provides a thread-safe storage for Kubernetes API
+// clients connected to target clusters. The internal storage is updated
+// automatically by watching for Cluster and Secret resources. New Cluster
+// objects trigger a client/informer creation, updates to Secret objects trigger
+// re-creation of a client/informer, and Cluster deletions cause the removal of
+// a client and its associated informer.
 //
 // Important Note
 //
