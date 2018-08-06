c1d1c25d1f1b256d49587522e48e6daa3c917f8c
Igor Sutton <igor.sutton@booking.com>
2018-08-06 14:27:20 +0200

    strategy controller: add corner case for releases with one pod in total and multiple steps strategy

--- pkg/controller/strategy/comparisons.go
+++ pkg/controller/strategy/comparisons.go
@@ -1,11 +1,23 @@
 package strategy
 
-func contenderCapacityComparison(achieved uint, desired uint) bool {
-	return achieved >= desired
+func contenderCapacityComparison(achievedPercentage, desiredPercentage, totalNumberOfPods uint) bool {
+	// Consider that contender capacity has achieved its desired state if
+	// the end game is one pod and we already have it, as long as desired
+	// is greater than zero.
+	if totalNumberOfPods == 1 && achievedPercentage == 100 && desiredPercentage > 0 {
+		return true
+	}
+	return achievedPercentage >= desiredPercentage
 }
 
-func incumbentCapacityComparison(achieved uint, desired uint) bool {
-	return achieved <= desired
+func incumbentCapacityComparison(achievedPercentage, desiredPercentage, totalNumberOfPods uint) bool {
+	// Consider that the incumbent capacity has achieved its desired state if
+	// the end game is one pod and we already have it, as long as the desired
+	// is smaller than 100.
+	if totalNumberOfPods == 1 && achievedPercentage == 100 && desiredPercentage < 100 {
+		return true
+	}
+	return achievedPercentage <= desiredPercentage
 }
 
 func contenderTrafficComparison(achieved uint32, desired uint32) bool {
