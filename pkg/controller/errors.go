fb9cec30aa425cb9e8a6d44eb26f623cf5621755
Igor Sutton <igor.sutton@booking.com>
2018-08-06 16:34:51 +0200

    errors: fix internal errors by aliasing to string instead of error interface

--- pkg/controller/errors.go
+++ pkg/controller/errors.go
@@ -6,7 +6,11 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
-type MultipleOwnerReferencesError error
+type MultipleOwnerReferencesError string
+
+func (e MultipleOwnerReferencesError) Error() string {
+	return string(e)
+}
 
 func IsMultipleOwnerReferencesError(err error) bool {
 	_, ok := err.(MultipleOwnerReferencesError)
@@ -14,12 +18,16 @@ func IsMultipleOwnerReferencesError(err error) bool {
 }
 
 func NewMultipleOwnerReferencesError(name string, references int) MultipleOwnerReferencesError {
-	return MultipleOwnerReferencesError(fmt.Errorf(
+	return MultipleOwnerReferencesError(fmt.Sprintf(
 		"expected exactly one owner for object %q, got %d",
 		name, references))
 }
 
-type WrongOwnerReferenceError error
+type WrongOwnerReferenceError string
+
+func (e WrongOwnerReferenceError) Error() string {
+	return string(e)
+}
 
 func IsWrongOwnerReferenceError(err error) bool {
 	_, ok := err.(WrongOwnerReferenceError)
@@ -27,7 +35,7 @@ func IsWrongOwnerReferenceError(err error) bool {
 }
 
 func NewWrongOwnerReferenceError(name string, expectedUID, gotUID types.UID) WrongOwnerReferenceError {
-	return WrongOwnerReferenceError(fmt.Errorf(
+	return WrongOwnerReferenceError(fmt.Sprintf(
 		"the owner Release for InstallationTarget %q is gone; expected UID %s but got %s",
 		name,
 		expectedUID,
