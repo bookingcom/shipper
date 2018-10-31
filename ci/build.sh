90e0549b8d236e2be52e80a24971fd03b58b2207
Ben Tyler <benjamin.tyler@gmail.com>
2018-10-31 12:54:08 +0100

    ci: add -e to test.sh (#33)
    * ci: add -e to test.sh
    
    'go test' exiting non-zero was not causing the build to fail
    
    * ci: also add -e to build.sh
    
    The 'go test' in test.sh should always catch build failures, but better
    safe than sorry.

--- ci/build.sh
+++ ci/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 
 CGO_ENABLED=0 GOOS=linux go build -v -o shipper cmd/shipper/*.go
 CGO_ENABLED=0 GOOS=linux go build -v -o shipper-state-metrics cmd/shipper-state-metrics/*.go
--- ci/test.sh
+++ ci/test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 
 dep status -v
 
