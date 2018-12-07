da6ebba67411e90b9790ca4ee327016e5a1551bc
Ivan Kruglov <ivan.kruglov@booking.com>
2018-12-07 16:05:32 +0100

    add shipperctl to the list of binaries to build

--- ci/build.sh
+++ ci/build.sh
@@ -2,3 +2,4 @@
 
 CGO_ENABLED=0 GOOS=linux go build -v -o shipper cmd/shipper/*.go
 CGO_ENABLED=0 GOOS=linux go build -v -o shipper-state-metrics cmd/shipper-state-metrics/*.go
+CGO_ENABLED=0 GOOS=linux go build -v -o shipperctl cmd/shipperctl/*.go
