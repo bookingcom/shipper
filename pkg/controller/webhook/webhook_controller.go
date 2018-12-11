6622cbe96e90830c937163e6c55568463c4d3497
Igor Sutton <igor.sutton@booking.com>
2018-12-11 11:43:33 +0100

    web-hook: controller now starts and stops with other controllers

--- pkg/controller/webhook/webhook_controller.go
+++ pkg/controller/webhook/webhook_controller.go
@@ -2,6 +2,7 @@ package webhook
 
 import (
 	"context"
+	"fmt"
 	"github.com/golang/glog"
 	"net/http"
 )
@@ -15,7 +16,11 @@ type Controller struct {
 }
 
 func NewController() *Controller {
-	return &Controller{}
+	return &Controller{
+		bindAddr: "0.0.0.0",
+		// bindPort must be 443 even if not serving TLS.
+		bindPort: "9443",
+	}
 }
 
 func (c *Controller) Run(stopCh <-chan struct{}) {
@@ -23,20 +28,24 @@ func (c *Controller) Run(stopCh <-chan struct{}) {
 	mux := c.initializeHandlers()
 	server := &http.Server{Addr: addr, Handler: mux}
 
-	var serverError error = nil
-	if c.tlsCertFile == "" || c.tlsPrivateKeyFile == "" {
-		serverError = server.ListenAndServe()
-	} else {
-		serverError = server.ListenAndServeTLS(c.tlsCertFile, c.tlsPrivateKeyFile)
-	}
+	go func() {
+		var serverError error = nil
+		if c.tlsCertFile == "" || c.tlsPrivateKeyFile == "" {
+			serverError = server.ListenAndServe()
+		} else {
+			serverError = server.ListenAndServeTLS(c.tlsCertFile, c.tlsPrivateKeyFile)
+		}
 
-	if serverError != nil {
-		glog.Fatalf("failed to start shipper-webhook-controller: %v", serverError)
-	}
+		if serverError != nil {
+			glog.Fatalf("failed to start shipper-webhook-controller: %v", serverError)
+		}
+	}()
 
-	glog.V(4).Info("Started WebHook controller")
+	glog.V(2).Info("Started WebHook controller")
 
 	<-stopCh
+
+	glog.V(2).Info("Shutting down WebHook controller")
 
 	if err := server.Shutdown(context.Background()); err != nil {
 		glog.Errorf(`HTTP server Shutdown: %v`, err)
