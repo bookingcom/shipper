81ae4a5d5bd52426223031396dddcd7462aa9da2
Igor Sutton <igor.sutton@booking.com>
2019-02-13 15:39:10 +0100

    reporting: create builders to aid testing

--- pkg/controller/capacity/capacity_controller_test.go
+++ pkg/controller/capacity/capacity_controller_test.go
@@ -61,53 +61,47 @@ func TestCapacityTargetStatusReturnsCorrectFleetReportWithSinglePod(t *testing.T
 
 	deployment := newDeployment(1, 1)
 	podLabels, _ := metav1.LabelSelectorAsMap(deployment.Spec.Selector)
-	podA := &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "pod-a",
-			Namespace: capacityTarget.GetNamespace(),
-			Labels:    podLabels,
-		},
-		Status: corev1.PodStatus{
-			ContainerStatuses: []corev1.ContainerStatus{
-				{
-					Name:  "app",
-					State: corev1.ContainerState{Running: &corev1.ContainerStateRunning{}},
-				},
-			},
-			Conditions: []corev1.PodCondition{
-				{
-					Type: corev1.PodReady,
-				},
-			},
-		},
-	}
+
+	podA := newPodBuilder("pod-a", deployment.GetNamespace(), podLabels).
+		AddContainerStatus("app", corev1.ContainerState{Running: &corev1.ContainerStateRunning{}}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodInitialized, Status: corev1.ConditionTrue}).
+		AddPodCondition(corev1.PodCondition{Type: "ContainersReady", Status: corev1.ConditionTrue}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodReady, Status: corev1.ConditionTrue}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodScheduled, Status: corev1.ConditionTrue}).
+		Build()
+
 	f.targetClusterObjects = append(f.targetClusterObjects, deployment, podA)
 
-	c := shipper.ClusterCapacityReport{
-		Owner: shipper.ClusterCapacityReportOwner{
-			Name: "nginx",
-		},
-		Breakdown: []shipper.ClusterCapacityReportBreakdown{
-			{
-				Count: 1,
-				Type:  string(corev1.PodReady),
-				Containers: []shipper.ClusterCapacityReportContainerBreakdown{
-					{
-						Name: "app",
-						States: []shipper.ClusterCapacityReportContainerStateBreakdown{
-							{
-								Count: 1,
-								Type:  "Running",
-								Example: shipper.ClusterCapacityReportContainerBreakdownExample{
-									Pod: "pod-a",
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
+	c := newReportBuilder("nginx").
+		AddBreakdown(
+			newBreakdownBuilder(1, "ContainersReady", string(corev1.ConditionTrue), "").
+				AddContainerBreakdown(
+					newContainerBreakdownBuilder("app").
+						AddState(1, "pod-a", "Running", "").
+						Build()).
+				Build()).
+		AddBreakdown(
+			newBreakdownBuilder(1, string(corev1.PodInitialized), string(corev1.ConditionTrue), "").
+				AddContainerBreakdown(
+					newContainerBreakdownBuilder("app").
+						AddState(1, "pod-a", "Running", "").
+						Build()).
+				Build()).
+		AddBreakdown(
+			newBreakdownBuilder(1, string(corev1.PodScheduled), string(corev1.ConditionTrue), "").
+				AddContainerBreakdown(
+					newContainerBreakdownBuilder("app").
+						AddState(1, "pod-a", "Running", "").
+						Build()).
+				Build()).
+		AddBreakdown(
+			newBreakdownBuilder(1, "Ready", string(corev1.ConditionTrue), "").
+				AddContainerBreakdown(
+					newContainerBreakdownBuilder("app").
+						AddState(1, "pod-a", "Running", "").
+						Build()).
+				Build()).
+		Build()
 
 	f.managementObjects = append(f.managementObjects, capacityTarget.DeepCopy())
 
@@ -147,73 +141,55 @@ func TestCapacityTargetStatusReturnsCorrectFleetReportWithMultiplePods(t *testin
 
 	deployment := newDeployment(2, 2)
 	podLabels, _ := metav1.LabelSelectorAsMap(deployment.Spec.Selector)
-	podA := &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "pod-a",
-			Namespace: capacityTarget.GetNamespace(),
-			Labels:    podLabels,
-		},
-		Status: corev1.PodStatus{
-			ContainerStatuses: []corev1.ContainerStatus{
-				{
-					Name:  "app",
-					State: corev1.ContainerState{Running: &corev1.ContainerStateRunning{}},
-				},
-			},
-			Conditions: []corev1.PodCondition{
-				{
-					Type: corev1.PodReady,
-				},
-			},
-		},
-	}
-	podB := &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "pod-b",
-			Namespace: capacityTarget.GetNamespace(),
-			Labels:    podLabels,
-		},
-		Status: corev1.PodStatus{
-			ContainerStatuses: []corev1.ContainerStatus{
-				{
-					Name:  "app",
-					State: corev1.ContainerState{Running: &corev1.ContainerStateRunning{}},
-				},
-			},
-			Conditions: []corev1.PodCondition{
-				{
-					Type: corev1.PodReady,
-				},
-			},
-		},
-	}
+
+	podA := newPodBuilder("pod-a", deployment.GetNamespace(), podLabels).
+		AddContainerStatus("app", corev1.ContainerState{Running: &corev1.ContainerStateRunning{}}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodInitialized, Status: corev1.ConditionTrue}).
+		AddPodCondition(corev1.PodCondition{Type: "ContainersReady", Status: corev1.ConditionTrue}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodReady, Status: corev1.ConditionTrue}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodScheduled, Status: corev1.ConditionTrue}).
+		Build()
+
+	podB := newPodBuilder("pod-b", deployment.GetNamespace(), podLabels).
+		AddContainerStatus("app", corev1.ContainerState{Running: &corev1.ContainerStateRunning{}}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodInitialized, Status: corev1.ConditionTrue}).
+		AddPodCondition(corev1.PodCondition{Type: "ContainersReady", Status: corev1.ConditionTrue}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodReady, Status: corev1.ConditionTrue}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodScheduled, Status: corev1.ConditionTrue}).
+		Build()
+
 	f.targetClusterObjects = append(f.targetClusterObjects, deployment, podA, podB)
 
-	c := shipper.ClusterCapacityReport{
-		Owner: shipper.ClusterCapacityReportOwner{
-			Name: "nginx",
-		},
-		Breakdown: []shipper.ClusterCapacityReportBreakdown{
-			{
-				Count: 2,
-				Type:  string(corev1.PodReady),
-				Containers: []shipper.ClusterCapacityReportContainerBreakdown{
-					{
-						Name: "app",
-						States: []shipper.ClusterCapacityReportContainerStateBreakdown{
-							{
-								Count: 2,
-								Type:  "Running",
-								Example: shipper.ClusterCapacityReportContainerBreakdownExample{
-									Pod: "pod-a",
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
+	c := newReportBuilder("nginx").
+		AddBreakdown(
+			newBreakdownBuilder(2, "ContainersReady", string(corev1.ConditionTrue), "").
+				AddContainerBreakdown(
+					newContainerBreakdownBuilder("app").
+						AddState(2, "pod-a", "Running", "").
+						Build()).
+				Build()).
+		AddBreakdown(
+			newBreakdownBuilder(2, string(corev1.PodInitialized), string(corev1.ConditionTrue), "").
+				AddContainerBreakdown(
+					newContainerBreakdownBuilder("app").
+						AddState(2, "pod-a", "Running", "").
+						Build()).
+				Build()).
+		AddBreakdown(
+			newBreakdownBuilder(2, string(corev1.PodScheduled), string(corev1.ConditionTrue), "").
+				AddContainerBreakdown(
+					newContainerBreakdownBuilder("app").
+						AddState(2, "pod-a", "Running", "").
+						Build()).
+				Build()).
+		AddBreakdown(
+			newBreakdownBuilder(2, "Ready", string(corev1.ConditionTrue), "").
+				AddContainerBreakdown(
+					newContainerBreakdownBuilder("app").
+						AddState(2, "pod-a", "Running", "").
+						Build()).
+				Build()).
+		Build()
 
 	f.managementObjects = append(f.managementObjects, capacityTarget.DeepCopy())
 
@@ -253,192 +229,119 @@ func TestCapacityTargetStatusReturnsCorrectFleetReportWithMultiplePodsWithDiffer
 
 	deployment := newDeployment(3, 3)
 	podLabels, _ := metav1.LabelSelectorAsMap(deployment.Spec.Selector)
-	podA := &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "pod-a",
-			Namespace: capacityTarget.GetNamespace(),
-			Labels:    podLabels,
-		},
-		Status: corev1.PodStatus{
-			ContainerStatuses: []corev1.ContainerStatus{
-				{
-					Name:  "app",
-					State: corev1.ContainerState{Waiting: &corev1.ContainerStateWaiting{Reason: "ContainerCreating"}},
-				},
-			},
-			Conditions: []corev1.PodCondition{
-				{
-					Type:   corev1.PodScheduled,
-					Status: corev1.ConditionTrue,
-				},
-				{
-					Type:   corev1.PodInitialized,
-					Status: corev1.ConditionTrue,
-				},
-				{
-					Type:   corev1.PodReady,
-					Status: corev1.ConditionFalse,
-					Reason: "ContainersNotReady",
-				},
-			},
-		},
-	}
-	podB := &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "pod-b",
-			Namespace: capacityTarget.GetNamespace(),
-			Labels:    podLabels,
-		},
-		Status: corev1.PodStatus{
-			ContainerStatuses: []corev1.ContainerStatus{
-				{
-					Name:  "app",
-					State: corev1.ContainerState{Terminated: &corev1.ContainerStateTerminated{Reason: "Completed"}},
-				},
-			},
-			Conditions: []corev1.PodCondition{
-				{
-					Type:   corev1.PodScheduled,
-					Status: corev1.ConditionTrue,
-				},
-				{
-					Type:   corev1.PodInitialized,
-					Status: corev1.ConditionTrue,
-				},
-				{
-					Type:   corev1.PodReady,
-					Status: corev1.ConditionFalse,
-					Reason: "ContainersNotReady",
-				},
-			},
-		},
-	}
-	podC := &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "pod-c",
-			Namespace: capacityTarget.GetNamespace(),
-			Labels:    podLabels,
-		},
-		Status: corev1.PodStatus{
-			ContainerStatuses: []corev1.ContainerStatus{
-				{
-					Name:  "app",
-					State: corev1.ContainerState{Waiting: &corev1.ContainerStateWaiting{Reason: "ContainerCreating"}},
-				},
-			},
-			Conditions: []corev1.PodCondition{
-				{
-					Type:   corev1.PodScheduled,
-					Status: corev1.ConditionTrue,
-				},
-				{
-					Type:   corev1.PodInitialized,
-					Status: corev1.ConditionTrue,
-				},
-				{
-					Type:   corev1.PodReady,
-					Status: corev1.ConditionFalse,
-					Reason: "ContainersNotReady",
-				},
-			},
-		},
-	}
+
+	podA := newPodBuilder("pod-a", deployment.GetNamespace(), podLabels).
+		AddContainerStatus("app", corev1.ContainerState{Waiting: &corev1.ContainerStateWaiting{Reason: "ContainerCreating"}}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodInitialized, Status: corev1.ConditionTrue}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodReady, Status: corev1.ConditionFalse, Reason: "ContainersNotReady"}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodScheduled, Status: corev1.ConditionTrue}).
+		Build()
+
+	podB := newPodBuilder("pod-b", deployment.GetNamespace(), podLabels).
+		AddContainerStatus("app", corev1.ContainerState{Waiting: &corev1.ContainerStateWaiting{Reason: "ContainerCreating"}}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodInitialized, Status: corev1.ConditionTrue}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodReady, Status: corev1.ConditionFalse, Reason: "ContainersNotReady"}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodScheduled, Status: corev1.ConditionTrue}).
+		Build()
+
+	podC := newPodBuilder("pod-c", deployment.GetNamespace(), podLabels).
+		AddContainerStatus("app", corev1.ContainerState{Terminated: &corev1.ContainerStateTerminated{Reason: "Completed"}}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodInitialized, Status: corev1.ConditionTrue}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodReady, Status: corev1.ConditionFalse, Reason: "ContainersNotReady"}).
+		AddPodCondition(corev1.PodCondition{Type: corev1.PodScheduled, Status: corev1.ConditionTrue}).
+		Build()
+
 	f.targetClusterObjects = append(f.targetClusterObjects, deployment, podA, podB, podC)
 
-	c := shipper.ClusterCapacityReport{
-		Owner: shipper.ClusterCapacityReportOwner{
-			Name: "nginx",
-		},
-		Breakdown: []shipper.ClusterCapacityReportBreakdown{
-			{
-				Count:  3,
-				Type:   string(corev1.PodInitialized),
-				Status: string(corev1.ConditionTrue),
-				Containers: []shipper.ClusterCapacityReportContainerBreakdown{
-					{
-						Name: "app",
-						States: []shipper.ClusterCapacityReportContainerStateBreakdown{
-							{
-								Count: 1,
-								Example: shipper.ClusterCapacityReportContainerBreakdownExample{
-									Pod: "pod-b",
-								},
-								Reason: "Completed",
-								Type:   "Terminated",
-							},
-							{
-								Count: 2,
-								Example: shipper.ClusterCapacityReportContainerBreakdownExample{
-									Pod: "pod-a",
-								},
-								Reason: "ContainerCreating",
-								Type:   "Waiting",
-							},
-						},
-					},
-				},
-			},
-			{
-				Count:  3,
-				Type:   string(corev1.PodScheduled),
-				Status: string(corev1.ConditionTrue),
-				Containers: []shipper.ClusterCapacityReportContainerBreakdown{
-					{
-						Name: "app",
-						States: []shipper.ClusterCapacityReportContainerStateBreakdown{
-							{
-								Count: 1,
-								Example: shipper.ClusterCapacityReportContainerBreakdownExample{
-									Pod: "pod-b",
-								},
-								Type:   "Terminated",
-								Reason: "Completed",
-							},
-							{
-								Count: 2,
-								Example: shipper.ClusterCapacityReportContainerBreakdownExample{
-									Pod: "pod-a",
-								},
-								Type:   "Waiting",
-								Reason: "ContainerCreating",
-							},
-						},
-					},
-				},
-			},
-			{
-				Count:  3,
-				Type:   string(corev1.PodReady),
-				Status: string(corev1.ConditionFalse),
+	c := newReportBuilder("nginx").
+		AddBreakdown(
+			newBreakdownBuilder(3, string(corev1.PodInitialized), string(corev1.ConditionTrue), "").
+				AddContainerBreakdown(
+					newContainerBreakdownBuilder("app").
+						AddState(2, "pod-a", "Waiting", "ContainerCreating").
+						AddState(1, "pod-c", "Terminated", "Completed").
+						Build()).
+				Build()).
+		AddBreakdown(
+			newBreakdownBuilder(3, string(corev1.PodReady), string(corev1.ConditionFalse), "ContainersNotReady").
+				AddContainerBreakdown(
+					newContainerBreakdownBuilder("app").
+						AddState(2, "pod-a", "Waiting", "ContainerCreating").
+						AddState(1, "pod-c", "Terminated", "Completed").
+						Build()).
+				Build()).
+		AddBreakdown(
+			newBreakdownBuilder(3, string(corev1.PodScheduled), string(corev1.ConditionTrue), "").
+				AddContainerBreakdown(
+					newContainerBreakdownBuilder("app").
+						AddState(2, "pod-a", "Waiting", "ContainerCreating").
+						AddState(1, "pod-c", "Terminated", "Completed").
+						Build()).
+				Build()).
+		Build()
+
+	f.managementObjects = append(f.managementObjects, capacityTarget.DeepCopy())
+
+	sadPodsStatuses := []shipper.PodStatus{
+		{
+			Condition: corev1.PodCondition{
+				Type:   corev1.PodReady,
+				Status: corev1.ConditionFalse,
 				Reason: "ContainersNotReady",
-				Containers: []shipper.ClusterCapacityReportContainerBreakdown{
-					{
-						Name: "app",
-						States: []shipper.ClusterCapacityReportContainerStateBreakdown{
-							{
-								Count:  1,
-								Type:   "Terminated",
-								Reason: "Completed",
-								Example: shipper.ClusterCapacityReportContainerBreakdownExample{
-									Pod: "pod-b",
-								},
-							},
-							{
-								Count:  2,
-								Type:   "Waiting",
-								Reason: "ContainerCreating",
-								Example: shipper.ClusterCapacityReportContainerBreakdownExample{
-									Pod: "pod-a",
-								},
-							},
+			},
+			Containers: []corev1.ContainerStatus{
+				{
+					Name: "app",
+					State: corev1.ContainerState{
+						Waiting: &corev1.ContainerStateWaiting{
+							Reason: "ContainerCreating",
 						},
 					},
 				},
 			},
+			Name: "pod-a",
+		},
+		{
+			Condition: corev1.PodCondition{
+				Type:   corev1.PodReady,
+				Status: corev1.ConditionFalse,
+				Reason: "ContainersNotReady",
+			},
+			Containers: []corev1.ContainerStatus{
+				{
+					Name: "app",
+					State: corev1.ContainerState{
+						Waiting: &corev1.ContainerStateWaiting{
+							Reason: "ContainerCreating",
+						},
+					},
+				},
+			},
+			Name: "pod-b",
+		},
+		{
+			Condition: corev1.PodCondition{
+				Type:   corev1.PodReady,
+				Status: corev1.ConditionFalse,
+				Reason: "ContainersNotReady",
+			},
+			Containers: []corev1.ContainerStatus{
+				{
+					Name: "app",
+					State: corev1.ContainerState{
+						Terminated: &corev1.ContainerStateTerminated{
+							Reason: "Completed",
+						},
+					},
+				},
+			},
+			Name: "pod-c",
 		},
 	}
 
-	f.managementObjects = append(f.managementObjects, capacityTarget.DeepCopy())
+	sort.Slice(sadPodsStatuses, func(i, j int) bool {
+		return sadPodsStatuses[i].Name < sadPodsStatuses[j].Name
+	})
 
 	capacityTarget.Status.Clusters = append(capacityTarget.Status.Clusters, shipper.ClusterCapacityStatus{
 		Name:              "minikube",
@@ -448,62 +351,7 @@ func TestCapacityTargetStatusReturnsCorrectFleetReportWithMultiplePodsWithDiffer
 		Conditions: []shipper.ClusterCapacityCondition{
 			{Type: shipper.ClusterConditionTypeReady, Status: corev1.ConditionFalse, Reason: conditions.PodsNotReady, Message: "there are 3 sad pods"},
 		},
-		SadPods: []shipper.PodStatus{
-			{
-				Condition: corev1.PodCondition{
-					Type:   corev1.PodReady,
-					Status: corev1.ConditionFalse,
-					Reason: "ContainersNotReady",
-				},
-				Containers: []corev1.ContainerStatus{
-					{
-						Name: "app",
-						State: corev1.ContainerState{
-							Terminated: &corev1.ContainerStateTerminated{
-								Reason: "Completed",
-							},
-						},
-					},
-				},
-				Name: "pod-b",
-			},
-			{
-				Condition: corev1.PodCondition{
-					Type:   corev1.PodReady,
-					Status: corev1.ConditionFalse,
-					Reason: "ContainersNotReady",
-				},
-				Containers: []corev1.ContainerStatus{
-					{
-						Name: "app",
-						State: corev1.ContainerState{
-							Waiting: &corev1.ContainerStateWaiting{
-								Reason: "ContainerCreating",
-							},
-						},
-					},
-				},
-				Name: "pod-a",
-			},
-			{
-				Condition: corev1.PodCondition{
-					Type:   corev1.PodReady,
-					Status: corev1.ConditionFalse,
-					Reason: "ContainersNotReady",
-				},
-				Containers: []corev1.ContainerStatus{
-					{
-						Name: "app",
-						State: corev1.ContainerState{
-							Waiting: &corev1.ContainerStateWaiting{
-								Reason: "ContainerCreating",
-							},
-						},
-					},
-				},
-				Name: "pod-c",
-			},
-		},
+		SadPods: sadPodsStatuses,
 	})
 
 	sort.Slice(capacityTarget.Status.Clusters[0].SadPods, func(i, j int) bool {
---
+++ pkg/controller/capacity/utils_test.go
@@ -0,0 +1,72 @@
+package capacity
+
+import (
+	"sort"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type PodBuilder struct {
+	podName           string
+	podNamespace      string
+	podLabels         map[string]string
+	containerStatuses []corev1.ContainerStatus
+	podConditions     []corev1.PodCondition
+}
+
+func newPodBuilder(podName string, podNamespace string, podLabels map[string]string) *PodBuilder {
+	return &PodBuilder{
+		podName:      podName,
+		podNamespace: podNamespace,
+		podLabels:    podLabels,
+	}
+}
+
+func (p *PodBuilder) SetName(name string) *PodBuilder {
+	p.podName = name
+	return p
+}
+
+func (p *PodBuilder) SetNamespace(namespace string) *PodBuilder {
+	p.podNamespace = namespace
+	return p
+}
+
+func (p *PodBuilder) SetLabels(labels map[string]string) *PodBuilder {
+	p.podLabels = labels
+	return p
+}
+
+func (p *PodBuilder) AddContainerStatus(containerName string, containerState corev1.ContainerState) *PodBuilder {
+	p.containerStatuses = append(p.containerStatuses, corev1.ContainerStatus{Name: containerName, State: containerState})
+	return p
+}
+
+func (p *PodBuilder) AddPodCondition(cond corev1.PodCondition) *PodBuilder {
+	p.podConditions = append(p.podConditions, cond)
+	return p
+}
+
+func (p *PodBuilder) Build() *corev1.Pod {
+
+	sort.Slice(p.podConditions, func(i, j int) bool {
+		return p.podConditions[i].Type < p.podConditions[j].Type
+	})
+
+	sort.Slice(p.containerStatuses, func(i, j int) bool {
+		return p.containerStatuses[i].Name < p.containerStatuses[j].Name
+	})
+
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      p.podName,
+			Namespace: p.podNamespace,
+			Labels:    p.podLabels,
+		},
+		Status: corev1.PodStatus{
+			ContainerStatuses: p.containerStatuses,
+			Conditions:        p.podConditions,
+		},
+	}
+}
