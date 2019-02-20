717abb556e0e3d1f6f76a73292653666e90354ef
Igor Sutton <igor.sutton@booking.com>
2019-02-20 10:11:44 +0100

    report: organize import order and add import aliases

--- pkg/controller/capacity/reporting.go
+++ pkg/controller/capacity/reporting.go
@@ -1,14 +1,15 @@
 package capacity
 
 import (
-	"github.com/bookingcom/shipper/pkg/controller/capacity/builder"
-	core_v1 "k8s.io/api/core/v1"
 	"sort"
 
-	shipper_v1alpha1 "github.com/bookingcom/shipper/pkg/apis/shipper/v1alpha1"
+	core_v1 "k8s.io/api/core/v1"
+
+	shipper "github.com/bookingcom/shipper/pkg/apis/shipper/v1alpha1"
+	"github.com/bookingcom/shipper/pkg/controller/capacity/builder"
 )
 
-func buildReport(ownerName string, podsList []*core_v1.Pod) *shipper_v1alpha1.ClusterCapacityReport {
+func buildReport(ownerName string, podsList []*core_v1.Pod) *shipper.ClusterCapacityReport {
 
 	sort.Slice(podsList, func(i, j int) bool {
 		return podsList[i].Name < podsList[j].Name
