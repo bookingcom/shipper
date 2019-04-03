ce67d5da36d126f5d437ba90c5bccab0326d2abd
Parham Doustdar <parham.doustdar@booking.com>
2019-04-03 12:18:53 +0200

    Started using shipperctl

--- ci/e2e.sh
+++ ci/e2e.sh
@@ -4,15 +4,13 @@ set -e
 
 mkdir ~/.kube
 microk8s.config > ~/.kube/config
-kubectl create ns shipper-system
-perl hack/install-crds.pl
-go run cmd/create-cluster-secret/main.go --api-server http://127.0.0.1:8080
 
+go run cmd/shipperctl/main.go admin clusters apply -f ci/clusters.yaml
 
 go run cmd/shipper/*.go --kubeconfig ~/.kube/config --disable clustersecret --resync "$1" --log_dir /tmp &
 SHIPPER_PID=$!
 
-go test ./test/e2e --test.v --e2e --kubeconfig ~/.kube/config --testcharts $PWD/test/e2e/testdata/\*.tgz --progresstimeout=2m --appcluster local
+go test ./test/e2e --test.v --e2e --kubeconfig ~/.kube/config --testcharts $PWD/test/e2e/testdata/\*.tgz --progresstimeout=2m --appcluster microk8s
 TEST_STATUS=$?
 
 set +e
