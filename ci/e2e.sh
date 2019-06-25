6eb5ae3951f7f4edbd95a8a8bb00b40432adece4
juliogreff <juliogreff@gmail.com>
2019-06-25 12:42:49 +0200

    e2e: serve helm charts from the microk8s cluster
    Now that we have moved shipper to run in-cluster for e2e tests, the test
    charts are no longer reachable because the test server binds to
    127.0.0.1, in a way that's not exactly configurable without changing
    helm's repotest package.
    
    To make charts reachable again, we deploy an image with `helm serve` and
    our testdata directory to the cluster, and point the e2e tests to that.

--- ci/e2e.sh
+++ ci/e2e.sh
@@ -13,8 +13,15 @@ CGO_ENABLED=0 GOOS=linux go build -o shipper cmd/shipper/*.go
 docker build -t localhost:32000/bookingcom/shipper:$TRAVIS_COMMIT -f Dockerfile.shipper .
 docker push localhost:32000/bookingcom/shipper:$TRAVIS_COMMIT
 sed s=\<IMAGE\>=localhost:32000/bookingcom/shipper:$TRAVIS_COMMIT= kubernetes/shipper.deployment.yaml | kubectl create -f -
+# Build an image with the test charts and deploy it
+HELM_IMAGE=localhost:32000/bookingcom/shipper-helm:latest make helm
+kubectl apply -f ci/helm.yaml
 
-go test ./test/e2e --test.v --e2e --kubeconfig ~/.kube/config --testcharts $PWD/test/e2e/testdata/\*.tgz --progresstimeout=2m --appcluster microk8s
+TESTCHARTS=http://$(kubectl get pod -l app=helm -o jsonpath='{.items[0].status.podIP}'):8879
+
+go test ./test/e2e --test.v --e2e --kubeconfig ~/.kube/config \
+	--testcharts $TESTCHARTS --progresstimeout=2m --appcluster microk8s
+
 TEST_STATUS=$?
 
 set +e
