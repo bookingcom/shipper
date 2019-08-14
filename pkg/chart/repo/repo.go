c6399677188a5f4161aed9f1adeadd4bac8a306e
Oleg Sidorov <oleg.sidorov@booking.com>
2019-08-14 10:52:37 +0200

    Chart repo errors are wrapped in Shipper errors fixes #152
    This change is aiming to turn helm library errors into Shipper errors so
    the responsible caller gets an idea if the error should be retried.
    
    Signed-off-by: Oleg Sidorov <oleg.sidorov@booking.com>

--- pkg/chart/repo/repo.go
+++ pkg/chart/repo/repo.go
@@ -120,7 +120,7 @@ func (r *Repo) ResolveVersion(chartspec *shipper.Chart) (*repo.ChartVersion, err
 	}
 
 	if len(versions) == 0 {
-		return nil, repo.ErrNoChartVersion
+		return nil, shippererrors.NewChartVersionResolveError(chartspec, repo.ErrNoChartVersion)
 	}
 
 	return versions[0], nil
@@ -132,10 +132,10 @@ func (r *Repo) FetchChartVersions(chartspec *shipper.Chart) (repo.ChartVersions,
 
 	vs, ok := r.index.Load().(*repo.IndexFile).Entries[chartspec.Name]
 	if !ok {
-		return nil, repo.ErrNoChartName
+		return nil, shippererrors.NewChartVersionResolveError(chartspec, repo.ErrNoChartName)
 	}
 	if len(vs) == 0 {
-		return nil, repo.ErrNoChartVersion
+		return nil, shippererrors.NewChartVersionResolveError(chartspec, repo.ErrNoChartVersion)
 	}
 
 	var constraint *semver.Constraints
@@ -254,7 +254,7 @@ func (r *Repo) Fetch(chartspec *shipper.Chart) (*chart.Chart, error) {
 		}
 	}
 	if chartver == nil {
-		return nil, repo.ErrNoChartVersion
+		return nil, shippererrors.NewChartVersionResolveError(chartspec, repo.ErrNoChartVersion)
 	}
 
 	if chart, err := r.LoadCached(chartver); err == nil {
