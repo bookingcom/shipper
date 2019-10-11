1ff66b62ffbee89b22a18823a57ac8b053a9e606
Oleg Sidorov <me@whitebox.io>
2019-10-11 12:47:31 +0200

    Implemented condition util module that embeds condition diff logic
    This commit addresses the necessity of having per-condition diff
    declarations every time one wants to trace a condition change. A new
    module was introduced: util/condution, which implements a generic
    ConditionDiff structure instead of util/application.ConditioDiff.
    
    The new implementation applies ad-hoc condition type-specific logic and
    uses reflections quite actively. This might be not the best
    implementation for this functionality. On the other hand, this allows us
    to keep v1 api types declaration clear and not mixed up with
    supplementary structures.
    
    Signed-off-by: Oleg Sidorov <oleg.sidorov@booking.com>

--- pkg/util/application/conditions.go
+++ pkg/util/application/conditions.go
@@ -1,69 +1,15 @@
 package application
 
 import (
-	"fmt"
 	"sort"
-	"strings"
 
 	coreV1 "k8s.io/api/core/v1"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	shipper "github.com/bookingcom/shipper/pkg/apis/shipper/v1alpha1"
+	condutil "github.com/bookingcom/shipper/pkg/util/condition"
 	diffutil "github.com/bookingcom/shipper/pkg/util/diff"
 )
-
-type ConditionDiff struct {
-	cond1, cond2 *shipper.ApplicationCondition
-}
-
-var _ diffutil.Diff = (*ConditionDiff)(nil)
-
-func NewConditionDiff(cond1, cond2 *shipper.ApplicationCondition) *ConditionDiff {
-	return &ConditionDiff{cond1: cond1, cond2: cond2}
-}
-
-func (d *ConditionDiff) IsEmpty() bool {
-	if d == nil || (d.cond1 == nil && d.cond2 == nil) {
-		return true
-	}
-	if d.cond1 != nil && d.cond2 != nil {
-		return d.cond1.Type == d.cond2.Type &&
-			d.cond1.Message == d.cond2.Message &&
-			d.cond1.Reason == d.cond2.Reason &&
-			d.cond1.Status == d.cond2.Status
-	}
-	return false
-}
-
-func (d *ConditionDiff) String() string {
-	if d.IsEmpty() {
-		return ""
-	}
-	var cond1str, cond2str string
-	if d.cond1 != nil {
-		cond1str = condToString(d.cond1)
-	}
-	if d.cond2 != nil {
-		cond2str = condToString(d.cond2)
-	}
-	return fmt.Sprintf("[%s] -> [%s]", cond1str, cond2str)
-}
-
-func condToString(cond *shipper.ApplicationCondition) string {
-	if cond == nil {
-		return ""
-	}
-	b := strings.Builder{}
-	b.WriteString(fmt.Sprintf("%v", cond.Type))
-	b.WriteString(fmt.Sprintf(" %v", cond.Status))
-	if cond.Reason != "" {
-		b.WriteString(fmt.Sprintf(" %q", cond.Reason))
-	}
-	if cond.Message != "" {
-		b.WriteString(fmt.Sprintf(" %q", cond.Message))
-	}
-	return b.String()
-}
 
 var ConditionsShouldDiscardTimestamps = false
 
@@ -84,7 +30,7 @@ func NewApplicationCondition(condType shipper.ApplicationConditionType, status c
 func SetApplicationCondition(status *shipper.ApplicationStatus, condition shipper.ApplicationCondition) diffutil.Diff {
 	currentCond := GetApplicationCondition(*status, condition.Type)
 
-	diff := NewConditionDiff(currentCond, &condition)
+	diff := condutil.NewConditionDiff(currentCond, &condition)
 	if diff.IsEmpty() {
 		return nil
 	}
