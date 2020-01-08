a5d4dc5cea1d8391838b6c20a24b4648a6d09ae0
juliogreff <juliogreff@gmail.com>
2020-01-08 13:23:26 +0100

    conditions: remove unused constants
    Most of these have been moved into the controllers themselves, and while
    that's not necessarily ideal, it's much better than a random "utils"
    package.

--- pkg/util/conditions/constants.go
+++ pkg/util/conditions/constants.go
@@ -1,35 +1,12 @@
 package conditions
 
 const (
-	// Operational.
-	ServerError = "ServerError"
-
-	// Capacity Ready.
-	MissingDeployment  = "MissingDeployment"
-	TooManyDeployments = "TooManyDeployments"
-	PodsNotReady       = "PodsNotReady"
-	WrongPodCount      = "WrongPodCount"
-
-	MissingObjects = "MissingObjects"
-	InvalidObjects = "InvalidObjects"
-
-	MissingService = "MissingService"
-
-	UnknownError = "UnknownError"
-
-	InternalError = "InternalError"
-
+	ServerError              = "ServerError"
 	TargetClusterClientError = "TargetClusterClientError"
 
 	CreateReleaseFailed                 = "CreateReleaseFailed"
 	ChartVersionResolutionFailed        = "ChartVersionResolutionFailed"
-	FetchReleaseFailed                  = "FetchReleaseFailed"
 	BrokenReleaseGeneration             = "BrokenReleaseGeneration"
 	BrokenApplicationObservedGeneration = "BrokenApplicationObservedGeneration"
 	StrategyExecutionFailed             = "StrategyExecutionFailed"
-
-	ChartError  = "ChartError"
-	ClientError = "ClientError"
-
-	ClustersNotReady = "ClustersNotReady"
 )
