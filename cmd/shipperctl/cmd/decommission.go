b16c9665874ac2e4120843b592ebf2f43e039a6b
hihilla <hilla.guz@gmail.com>
2020-09-11 10:01:47 +0200

    adding a dry run flag to clean command

--- cmd/shipperctl/cmd/decommission.go
+++ cmd/shipperctl/cmd/decommission.go
@@ -18,6 +18,7 @@ import (
 
 var (
 	decommissionedClusters []string
+	dryrun                 bool
 
 	CleanCmd = &cobra.Command{
 		Use:   "clean",
@@ -56,6 +57,7 @@ func init() {
 			command.Printf("warning: could not mark %q for filename autocompletion: %s\n", kubeConfigFlagName, err)
 		}
 
+		command.Flags().BoolVar(&dryrun, "dryrun", false, "if true, only prints the objects that will be modifies/deleted")
 		command.Flags().StringVar(&managementClusterContext, "management-cluster-context", "", "the name of the context to use to communicate with the management cluster. defaults to the current one")
 		command.Flags().StringSliceVar(&decommissionedClusters, "decommissionedClusters", decommissionedClusters, "list of decommissioned clusters")
 	}
@@ -83,7 +85,7 @@ func runCleanCommand(cmd *cobra.Command, args []string) error {
 			continue
 		}
 		for _, rel := range releaseList.Items {
-			trueClusters := getFilteredSelectedClusters(rel)
+			trueClusters := getFilteredSelectedClusters(&rel)
 			if len(trueClusters) > 0 {
 				sort.Strings(trueClusters)
 
@@ -93,25 +95,33 @@ func runCleanCommand(cmd *cobra.Command, args []string) error {
 				}
 				rel.Annotations[shipper.ReleaseClustersAnnotation] = strings.Join(trueClusters, ",")
 				cmd.Printf("Editing annotations of release %s/%s to %s...", rel.Namespace, rel.Name, rel.Annotations[shipper.ReleaseClustersAnnotation])
-				_, err := configurator.ShipperClient.ShipperV1alpha1().Releases(ns.Name).Update(&rel)
-				if err != nil {
-					errList = append(errList, err.Error())
+				if !dryrun {
+					_, err := configurator.ShipperClient.ShipperV1alpha1().Releases(ns.Name).Update(&rel)
+					if err != nil {
+						errList = append(errList, err.Error())
+					}
+					cmd.Println("done")
+				} else {
+					cmd.Println("dryrun")
 				}
-				cmd.Println("done")
 				continue
 			}
-			isContender, err := isContender(rel, configurator)
+			isContender, err := isContender(&rel, configurator)
 			if err != nil {
 				errList = append(errList, err.Error())
 				continue
 			}
 			if len(trueClusters) == 0 && !isContender {
 				cmd.Printf("Deleting release %s/%s...", rel.Namespace, rel.Name)
-				err := configurator.ShipperClient.ShipperV1alpha1().Releases(ns.Name).Delete(rel.Name, &metav1.DeleteOptions{})
-				if err != nil {
-					errList = append(errList, err.Error())
+				if !dryrun {
+					err := configurator.ShipperClient.ShipperV1alpha1().Releases(ns.Name).Delete(rel.Name, &metav1.DeleteOptions{})
+					if err != nil {
+						errList = append(errList, err.Error())
+					}
+					cmd.Println("done")
+				} else {
+					cmd.Println("dryrun")
 				}
-				cmd.Println("done")
 			}
 		}
 	}
@@ -141,12 +151,12 @@ func runCountContenderCommand(cmd *cobra.Command, args []string) error {
 			continue
 		}
 		for _, app := range applicationList.Items {
-			contender, err := getContender(app, configurator)
+			contender, err := getContender(&app, configurator)
 			if err != nil {
 				errList = append(errList, err.Error())
 				continue
 			}
-			trueClusters := getFilteredSelectedClusters(*contender)
+			trueClusters := getFilteredSelectedClusters(contender)
 			if len(trueClusters) == 0 {
 				counter++
 			}
@@ -180,7 +190,7 @@ func runCountReleasesCommand(cmd *cobra.Command, args []string) error {
 			continue
 		}
 		for _, rel := range releaseList.Items {
-			trueClusters := getFilteredSelectedClusters(rel)
+			trueClusters := getFilteredSelectedClusters(&rel)
 			if len(trueClusters) == 0 {
 				counter++
 			}
@@ -194,8 +204,8 @@ func runCountReleasesCommand(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-func getFilteredSelectedClusters(rel shipper.Release) []string {
-	clusters := releaseutil.GetSelectedClusters(&rel)
+func getFilteredSelectedClusters(rel *shipper.Release) []string {
+	clusters := releaseutil.GetSelectedClusters(rel)
 	var trueClusters []string
 	for _, cluster := range clusters {
 		if !filters.SliceContainsString(decommissionedClusters, cluster) {
@@ -205,29 +215,29 @@ func getFilteredSelectedClusters(rel shipper.Release) []string {
 	return trueClusters
 }
 
-func isContender(rel shipper.Release, configurator *configurator.Cluster) (bool, error) {
+func isContender(rel *shipper.Release, configurator *configurator.Cluster) (bool, error) {
 	appName := rel.Labels[shipper.AppLabel]
 	app, err := configurator.ShipperClient.ShipperV1alpha1().Applications(rel.Namespace).Get(appName, metav1.GetOptions{})
 	if err != nil {
 		return false, err
 	}
-	contender, err := getContender(*app, configurator)
+	contender, err := getContender(app, configurator)
 	if err != nil {
 		return false, err
 	}
 	return contender.Name == rel.Name && contender.Namespace == rel.Namespace, nil
 }
 
-func getContender(app shipper.Application, configurator *configurator.Cluster) (*shipper.Release, error) {
+func getContender(app *shipper.Application, configurator *configurator.Cluster) (*shipper.Release, error) {
 	appName := app.Name
 	selector := labels.Set{shipper.AppLabel: appName}.AsSelector()
 	releaseList, err := configurator.ShipperClient.ShipperV1alpha1().Releases(app.Namespace).List(metav1.ListOptions{LabelSelector: selector.String()})
 	if err != nil {
 		return nil, err
 	}
-	var rels []*shipper.Release
-	for _, rel := range releaseList.Items {
-		rels = append(rels, &rel)
+	rels := make([]*shipper.Release, len(releaseList.Items))
+	for i, _ := range releaseList.Items {
+		rels[i] = &releaseList.Items[i]
 	}
 	rels = releaseutil.SortByGenerationDescending(rels)
 	contender, err := apputil.GetContender(appName, rels)
